d5fdba154cd5184da3d49b6be6155dfa9b47d560
Ryan <ryjen.pears@gmail.com>
2023-02-02 16:37:34 +0100

    prisma adapter added

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import prisma from "../../../lib/prismadb"
+import prisma from "../../../../lib/prismadb.ts"
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
