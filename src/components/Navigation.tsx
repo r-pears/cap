9b255c2713ea80fa6df0537c361176f100fd5862
Ryan <ryjen.pears@gmail.com>
2023-02-02 16:26:03 +0100

    authentication added

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Link from 'next/link'
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Navigation = () => {
+  const { data: session } = useSession();
+
   return (
     <div>
       <Link href={`/`}>Home</Link>
       <Link href={`/posts`}>Posts</Link>
+      {!session ? (
+        <button onClick={() => signIn()}>Sign in</button>
+      ) : (
+        <button onClick={() => signOut()}>Sign out</button>
+      )}
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import Link from 'next/link'
-import { useSession, signIn, signOut } from "next-auth/react";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const { data: session } = useSession();
-
   return (
     <>
       <Head>
@@ -16,31 +13,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
-          <div>
-      {!session ? (
-        <>
-          <p>Not signed in</p>
-          <br />
-          <button onClick={() => signIn()}>Sign in</button>
-        </>
-      ) : (
-        <main>
-          <div >
-            <h4>Signed in as {session.user.name}</h4>
-            <button onClick={() => signOut()}>Sign out</button>
-          </div>
-        </main>
-      )}
-    </div>
-      {/* <div>
+      <div>
         <h1>Welcome to...</h1>
         <h2>Guides, tips, and posts written by locals. The best way to learn about hidden gems in the area.</h2>
         <div>
           <Link href={`/posts`}><h3>Explore posts</h3></Link>
           <Link href={`/posts/new`}><h3>Add your own post</h3></Link>
-          <Link href={`/user`}><h3>Login</h3></Link>
         </div>
-      </div> */}
+      </div>
     </>
   )
 }
