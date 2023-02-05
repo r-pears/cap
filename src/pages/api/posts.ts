b66bbd8d744c5a5b4c68d2de6e8d414b29b923c3
Ryan <ryjen.pears@gmail.com>
2023-02-05 16:33:31 +0100

    get all posts api

---
+++ src/pages/api/posts.ts
@@ -0,0 +1,18 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default async function getAllPosts(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+
+  try {
+    const posts = await prisma.post.findMany()
+    res.status(200).json(posts)
+  } catch (error) {
+    console.log(`Error: ${error}`)
+  }
+}
