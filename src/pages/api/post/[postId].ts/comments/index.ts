b1ad634a6ff6fac004bc524375d83b16bd869a36
Ryan <ryjen.pears@gmail.com>
2023-02-15 19:29:11 +0100

    ability to add a form route

--- src/pages/api/post/[postId].ts/comments/index.ts
+++ src/pages/api/post/[postId].ts/comments/index.ts
@@ -1,6 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient } from '@prisma/client'
+import {
+  getSession,
+} from "next-auth/react";
 
 const prisma = new PrismaClient()
 
@@ -11,14 +14,33 @@ export default async function getAllPosts(
 
   const { id } = req.body;
 
-  try {
-    const post = await prisma.comment.findMany({
-      where: {
-        postId: id
-      }
-    })
-    res.status(200).json(post)
-  } catch (error) {
-    console.log(`Error: ${error}`)
+  if (req.method === 'GET') {
+    try {
+      const post = await prisma.comment.findMany({
+        where: {
+          postId: id
+        }
+      })
+      res.status(200).json(post)
+    } catch (error) {
+      console.log(`Error: ${error}`)
+    }    
+  } else if (req.method === 'POST') {
+  const { title, content, postId } = req.body;
+
+  console.log(req.body)
+
+  const session = await getSession({ req });
+  if (session && session.user && session?.user.email) {
+    const result = await prisma.comment.create({
+      data: {
+        post: postId,
+        title: title,
+        content: content,
+        author: { connect: { email: session?.user?.email } },
+      },
+    });
+    res.json(result);    
+    } 
   }
 }
