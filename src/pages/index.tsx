c87c995f100da14cc6537bb92944916948b8fbaf
Ryan <ryjen.pears@gmail.com>
2023-02-18 15:19:44 +0100

    homepage styling added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import Link from 'next/link'
+import main from '../styles/main.module.css';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,12 +15,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
-      <div>
-        <h1>Welcome to...</h1>
-        <h2>Guides, tips, and posts written by locals. The best way to learn about hidden gems in the area.</h2>
+      <div className={main.main}>
+        <h1 className={main.title}>Welcome to...</h1>
+        <h2 className={main.subtitle}>Guides, tips, and posts written by locals. The best way to learn about hidden gems in the area.</h2>
         <div>
-          <Link href={`/posts`}><h3>Explore posts</h3></Link>
-          <Link href={`/posts/new`}><h3>Add your own post</h3></Link>
+          <Link className={main.links} href={`/posts`}><h3 className={main.link}>Explore posts</h3></Link>
+          <Link className={main.links} href={`/posts/new`}><h3 className={main.link}>Add your own post</h3></Link>
         </div>
       </div>
     </>
