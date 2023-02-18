ed0f4db67234149d99b9c78bd5e23f2cc13546b0
Ryan <ryjen.pears@gmail.com>
2023-02-18 15:14:15 +0100

    navigation styling added

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import Link from 'next/link'
 import { useSession, signIn, signOut } from "next-auth/react";
 import navigation from './Navigation.module.css';
+import { useRouter } from 'next/router';
 
 const Navigation = () => {
   const { data: session } = useSession();
+  const router = useRouter();
+  const currentRoute = router.pathname;
 
   return (
     <div className={navigation.navbar}>
-      <Link data-cy='nav-home' href={`/`}>Home</Link>
-      <Link data-cy='nav-posts' href={`/posts`}>Posts</Link>
+      <div className={navigation.links}>
+        <Link className={currentRoute === '/' ? navigation.linkActive : navigation.link} data-cy='nav-home' href={`/`}>Home</Link>
+        <Link className={currentRoute.startsWith('/posts') ? navigation.linkActive : navigation.link} data-cy='nav-posts' href={`/posts`}>Posts</Link>        
+      </div>
       {!session ? (
-        <button onClick={() => signIn()}>Sign in</button>
+        <button className={navigation.button} onClick={() => signIn()}>Sign in</button>
       ) : (
-        <button onClick={() => signOut()}>Sign out</button>
+        <button className={navigation.button} onClick={() => signOut()}>Sign out</button>
       )}
     </div>
   )
